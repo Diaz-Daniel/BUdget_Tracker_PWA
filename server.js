eef16aa9c467c31f6c8927caf88db9a14e0481a0
DDiaz <daniel.diaz.0515@gmail.com>
2021-12-30 19:30:47 -0800

    added file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-//https://safe-ravine-76830.herokuapp.com/
+
 const PORT = 3000 || process.env.PORT;
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/Transaction";
@@ -28,5 +28,4 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
-
-//heroku link: https://frozen-mountain-09300.herokuapp.com/
+//https://safe-ravine-76830.herokuapp.com/
